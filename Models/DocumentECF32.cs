4fe52618206a6bd18d814b0bcdd682a8fcdf0c9a
Felix Antonio Carvajal <felix.carvajal@dpworld.com.do>
2022-11-16 15:12:35 -0400

    Adding project

---
+++ Models/DocumentECF32.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XsdConvertXml.Models;
+
+public class DocumentECF32
+{
+    private void CreateXML(XmlNode xsdNode, XmlElement element, ref XmlDocument xml)
+    {
+        if (xsdNode.HasChildNodes)
+        {
+            var childs = xsdNode.ChildNodes;
+
+            foreach (XmlNode node in childs)
+            {
+                XmlElement newElement = null;
+
+                if (node.Name == "xs:element")
+                {
+                    newElement = xml.CreateElement(node.Attributes["name"].Value);
+
+                    CreateXML(node, newElement, ref xml);
+
+                    if (element == null)
+                    {
+                        xml.AppendChild(newElement);
+                    }
+                    else
+                    {
+                        element.AppendChild(newElement);
+                    }
+                }
+
+                if (node.Name == "xs:attribute")
+                {
+                    element.SetAttribute(node.Attributes["name"].Value, "");
+                }
+
+                if ((node.Name == "xs:complexType") || 
+                    (node.Name == "xs:sequence") || 
+                    (node.Name == "xs:schema")) CreateXML(node, element, ref xml);
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml;
+using System.Xml.Schema;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        XmlSchemaSet schema = new XmlSchemaSet();
+        schema.Add("", @"Ruta del archivo XSD");
+
+        XmlReaderSettings settings = new XmlReaderSettings();
+        settings.ValidationType = ValidationType.Schema;
+        settings.Schemas = schema;
+        settings.ValidationEventHandler += ValidationEventHandler;
+
+        XmlReader reader = XmlReader.Create(@"Ruta del archivo XML", settings);
+
+        while (reader.Read()) ;
+    }
+
+    static void ValidationEventHandler(object sender, ValidationEventArgs e)
+    {
+        Console.WriteLine($"Error de Validacion:\n {e.Message}");
+    }
+}
